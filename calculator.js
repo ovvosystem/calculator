ef636082d05d7e134f1747ae10e41575fb855106
Solaris <ovvo.system@gmail.com>
2023-07-29 16:18:16 -0300

    Rewrite event listener into different functions
    Add pressNumber() and pressPoint() functions to use with the button
    event listener for better readability

--- calculator.js
+++ calculator.js
@@ -1,30 +1,33 @@
-const OPERATORS = ["+", "-", "*", "/"];
 let displayNumber = "0";
 let firstNumber;
 let secondNumber;
 let operator;
 
 const calcButtons = document.querySelectorAll(".calc-button");
-console.log(calcButtons);
 calcButtons.forEach(button => {
     button.addEventListener("click", event => {
         const buttonPressed = event.currentTarget;
+
         if (buttonPressed.classList.contains("number")) {
-            if (displayNumber == "0") displayNumber = button.textContent;
-            else displayNumber += button.textContent;
-        } else if (buttonPressed.id === "point") {
-            if (!displayNumber.includes(".")) displayNumber += button.textContent;
+            pressNumber(buttonPressed);
         }
+
+        else if (buttonPressed.id === "point") {
+            pressPoint(buttonPressed);
+        }
+
         updateDisplay(displayNumber);
     })
 })
 
 function operate(a, b, operator) {
+    a = +a;
+    b = +b;
     switch (operator) {
-        case "+": return a + b;
-        case "-": return a - b;
-        case "*": return a * b;
-        case "/": return a / b;
+        case "sum": return a + b;
+        case "subtract": return a - b;
+        case "multiply": return a * b;
+        case "divide": return a / b;
     }
 }
 
@@ -32,4 +35,13 @@ function updateDisplay(newDisplay) {
     if (newDisplay.length > 8) return;
     const display = document.getElementById("display");
     display.textContent = newDisplay;
+}
+
+function pressNumber(button) {
+    if (displayNumber == "0") displayNumber = button.textContent;
+    else displayNumber += button.textContent;
+}
+
+function pressPoint(button) {
+    if (!displayNumber.includes(".")) displayNumber += button.textContent;
 }
