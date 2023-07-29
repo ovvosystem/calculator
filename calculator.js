4fc1fff9a051934ba053efd68c85a296c430ec87
Solaris <ovvo.system@gmail.com>
2023-07-29 07:43:23 -0300

    Rewrite operate() function

--- calculator.js
+++ calculator.js
@@ -3,27 +3,11 @@ let firstNumber;
 let secondNumber;
 let operator;
 
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
 function operate(a, b, operator) {
     switch (operator) {
-        case "+": return add(a, b);
-        case "-": return subtract(a, b);
-        case "*": return multiply(a, b);
-        case "/": return divide(a, b);
+        case "+": return a + b;
+        case "-": return a - b;
+        case "*": return a * b;
+        case "/": return a / b;
     }
 }
