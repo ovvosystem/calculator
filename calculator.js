561b864035098000efb6960fb659ecedc77f6a9a
Solaris <ovvo.system@gmail.com>
2023-07-29 08:43:20 -0300

    Add event listener to buttons
    Updates the display when pressing a number or point

--- calculator.js
+++ calculator.js
@@ -4,6 +4,18 @@ let firstNumber;
 let secondNumber;
 let operator;
 
+const calcButtons = document.querySelectorAll(".calc-button");
+console.log(calcButtons);
+calcButtons.forEach(button => {
+    button.addEventListener("click", event => {
+        const buttonPressed = event.currentTarget;
+        if (buttonPressed.classList.contains("number") || buttonPressed.id === "point") {
+            displayNumber += button.textContent;
+            updateDisplay(displayNumber);
+        }
+    })
+})
+
 function operate(a, b, operator) {
     switch (operator) {
         case "+": return a + b;
